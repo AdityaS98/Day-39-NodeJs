f0ed9457aafc0d272c961da84d5e140457c7e8f0
AdityaS98 <adityasharmalko1998@gmail.com>
2023-06-13 07:10:52 +0530

    Added: Programs for checking employee attendance

---
+++ src/Employeecheck.js
@@ -0,0 +1,13 @@
+const emp_is_present =1;
+
+let empCheck = Math.floor(Math.random()*10)%2;
+
+console.log(emp_is_present);
+
+if(empCheck==emp_is_present){
+    
+    console.log("Employee is present for the given day");
+}
+else{
+    console.log("Employee is absent for the given day");
+}
